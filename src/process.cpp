a504c6b3d7ebffa72887a7a6fe62da1b64c54c3c
Gabor Jenei <jeneigabor14@gmail.com>
2024-08-27 21:37:26 +0100

    Update Process Class

--- src/process.cpp
+++ src/process.cpp
@@ -13,34 +13,38 @@ using std::vector;
 
 Process::Process(int pid) {
     pid_ = pid;
-    cpu_utilization_ = LinuxParser::ActiveJiffies(pid) / LinuxParser::Jiffies();
+    cpu_utilization_ = 0; 
+    cpu_utilization_ = float(LinuxParser::ActiveJiffies(pid)) / float(LinuxParser::Jiffies());
     command_ = Command();
+    ram_ = LinuxParser::Ram(pid);
+    uptime_ = LinuxParser::UpTime(pid);
+    user_ = LinuxParser::User(pid);
 }
 
-// TODO: Return this process's ID
+// Return this process's ID
 int Process::Pid() { return pid_; }
 
-// TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return cpu_utilization_; }
+// Return this process's CPU utilization
+float Process::CpuUtilization() const { return cpu_utilization_; }
 
-// TODO: Return the command that generated this process
+// Return the command that generated this process
 string Process::Command() {
     string line;
-    
     line = LinuxParser::Command(pid_);
-
     return line;
 }
 
-// TODO: Return this process's memory utilization
-string Process::Ram() { return string(); }
+// Return this process's memory utilization
+string Process::Ram() { return ram_; }
 
 // TODO: Return the user (name) that generated this process
-string Process::User() { return string(); }
+string Process::User() { return user_; }
 
 // TODO: Return the age of this process (in seconds)
-long int Process::UpTime() { return 0; }
+long int Process::UpTime() { return uptime_; }
 
 // TODO: Overload the "less than" comparison operator for Process objects
 // REMOVE: [[maybe_unused]] once you define the function
-bool Process::operator<(Process const& a[[maybe_unused]]) const { return true; }
+bool Process::operator<(Process const& a) const {
+    return (CpuUtilization() < a.CpuUtilization());
+    }
--- src/system.cpp
+++ src/system.cpp
@@ -22,7 +22,16 @@ Processor& System::Cpu() {
     }
 
 // TODO: Return a container composed of the system's processes
-vector<Process>& System::Processes() { return processes_; }
+vector<Process>& System::Processes() { 
+    vector<int> pids = LinuxParser::Pids();
+    // vector<Process> processes_;
+
+    for (int pid : pids) {
+        processes_.push_back(Process(pid));
+    }
+    
+    return processes_;
+}
 
 // Return the system's kernel identifier (string)
 std::string System::Kernel() { return LinuxParser::Kernel(); }
